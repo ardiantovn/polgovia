197d012dbb616b2f157d035e07bdecb044eec31e
ardiantovn <vendi2012@gmail.com>
2021-02-23 15:09:37 +0700

    u2

--- likeNG.py
+++ likeNG.py
@@ -1,3 +1,13 @@
-from livefuncNG import *
+from livefunc import *
 
-st.write("TEST")
+selectedOpsi= selectApp()
+headCont = st.beta_container()
+with headCont:
+    header()
+    about()
+if selectedOpsi == appOpsi[0]:
+    newsHead()
+    newsAppOpsi()
+elif selectedOpsi == appOpsi[1]:
+    twitterHead()
+    twitterAppOpsi()
